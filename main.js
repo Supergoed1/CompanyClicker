75f0972c62a3880cbda5ea1d5ea97dd615da8105
Supergoed1 <40697454+Supergoed1@users.noreply.github.com>
2019-04-18 08:58:41 +0200

    fkosdjf

--- main.js
+++ main.js
@@ -1,8 +1,36 @@
-var name = prompt("Name your website");
+var game = {
+    website: {
+        name: "DAB",
+        ad1: false,
+        ad2: false,
+        ad3: false,
+        ad4: false,
+        ad5: false
+    },
+    money: 0,
+
+};
+var updategui = setInterval("updateGUI()", 100);
 
 
 function init() {
-    document.getElementById("websiteTitle").innerHTML = name;
+    if(load() == false) {
+        game.website.name = prompt("Name your website")
+        save();
+    }
 }
 
+function save() {
+    localStorage.setItem("game", JSON.stringify(game));
+}
+
+function updateGUI() {
+    document.getElementById("websiteTitle").innerHTML = game.website.name;
+}
+
+function load() {
+    if(localStorage.length <= 0) return false;
+    if(localStorage.getItem("game") == null) return false;
+    game = JSON.parse(localStorage.getItem("game"));
+}
 init();
