3cda260ba3ddc3e3cbcdb41a4dcf5b6e528b4e8f
Supergoed1 <40697454+Supergoed1@users.noreply.github.com>
2019-04-20 10:54:34 +0200

    did stuff

--- main.js
+++ main.js
@@ -8,16 +8,31 @@ var game = {
         ad5: false
     },
     money: 0,
-
+    moneyPerClick: 0.01
 };
-var updategui = setInterval("updateGUI()", 100);
+var ad1c = document.getElementById("ad1content");
+var begButton = document.getElementById("beg");
 
+function beg() {
+    game.money += game.moneyPerClick;
+    updateGUI();
+}
+
+
+function checkads() {
+    if(game.website.ad1 == true) {
+        ad1c.style.display = "block";
+    } else if(game.website.ad1 == false) {
+        ad1c.style.display = "none";
+    }
+}
 
 function init() {
     if(load() == false) {
         game.website.name = prompt("Name your website")
         save();
     }
+    updateGUI();
 }
 
 function save() {
@@ -26,6 +41,9 @@ function save() {
 
 function updateGUI() {
     document.getElementById("websiteTitle").innerHTML = game.website.name;
+    document.getElementById("money").innerHTML = currencyFormat(game.money);
+    begButton.innerHTML = "+" + game.moneyPerClick;
+    checkads();
 }
 
 function load() {
@@ -33,4 +51,12 @@ function load() {
     if(localStorage.getItem("game") == null) return false;
     game = JSON.parse(localStorage.getItem("game"));
 }
+
+function currencyFormat(num) {
+    return '$' + num.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+  }
+
+window.onbeforeunload = function (){
+    save();
+};
 init();
